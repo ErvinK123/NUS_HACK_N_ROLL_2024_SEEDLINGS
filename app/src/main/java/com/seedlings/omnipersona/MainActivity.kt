389c7a36ea07ccde6fc3bca483833f97e7b3773c
Ervin Kin <ervinkin@yahoo.com.sg>
2024-01-20 13:40:43 +0800

    Set up project structure and main activity

--- app/src/main/java/com/seedlings/omnipersona/MainActivity.kt
+++ app/src/main/java/com/seedlings/omnipersona/MainActivity.kt
@@ -2,10 +2,18 @@ package com.seedlings.omnipersona
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 
+    private fun initWelcomeFragment() {
+        supportFragmentManager.commit {
+            
+        }
     }
 }
