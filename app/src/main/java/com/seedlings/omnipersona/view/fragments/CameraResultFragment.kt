8aaed8b3aa52101afdc92a431b44a53554bad3f9
24Donovan24 <donovanlee_25@hotmail.com>
2024-01-21 03:48:37 +0800

    Add mapping function

--- app/src/main/java/com/seedlings/omnipersona/view/fragments/CameraResultFragment.kt
+++ app/src/main/java/com/seedlings/omnipersona/view/fragments/CameraResultFragment.kt
@@ -21,6 +21,7 @@ class CameraResultFragment(private val curScore: MutableList<Int>, private val b
         super.onViewCreated(view, savedInstanceState)
         imageView = requireView().findViewById<ImageView>(R.id.testImage)
         imageView.setImageBitmap(bitmap)
+        classifyItem(picList)
 
         requireView().findViewById<Button>(R.id.photoNext).setOnClickListener {
             parentFragmentManager.commit{
@@ -36,4 +37,46 @@ class CameraResultFragment(private val curScore: MutableList<Int>, private val b
     override fun onStop() {
         super.onStop()
     }
+
+    private fun classifyItem(picList: List<String>) {
+        val personalities = mapOf(
+            0 to listOf(
+                "Bonfire", "Tractor", "Aquarium", "Circus", "Infrastructure", "Ferris wheel",
+                "Comet", "Mortarboard", "Track", "Christmas", "Rocket", "Rein", "Skiing", "Submarine"
+            ),
+            1 to listOf(
+                "Park", "Graduation", "Bridge", "Sitting", "Cushion", "Sunset", "Prom", "Reef",
+                "Picnic", "Wreath", "Ballroom", "Feather", "Swan", "Dove", "Squirrel"
+            ),
+            2 to listOf(
+                "Tights", "Bird", "Factory", "Twig", "Petal", "Sunglasses", "Watercolor paint",
+                "Competition", "Cliff", "Badminton", "Stadium", "Bus", "Sky", "Gerbil", "Menu",
+                "Cabinetry", "Laptop", "Robot", "Calculator", "Scale", "Microscope"
+            ),
+            3 to listOf(
+                "Bear", "Eating", "Tower", "Brick", "Junk", "Person", "Windsurfing", "Swimwear",
+                "Roller", "Camping", "Playground", "Laugh", "Balloon", "Concert", "Dance", "Safari"
+            ),
+            4 to listOf(
+                "Boat", "Bicycle", "Stadium", "Bullfighting", "Rock", "Interaction", "Dress", "Shorts",
+                "Surfboard", "Fast food", "Hot dog", "Sports car", "Tennis", "Running", "Basketball",
+                "Soccer", "Rugby", "Swimming", "Rowing", "Skateboarding", "Snowboarding", "Waterskiing"
+            ),
+            5 to listOf(
+                "Laugh", "Balloon", "Concert", "Prom", "Construction", "Product", "Reef", "Picnic",
+                "Wreath", "Wheelbarrow", "Book", "Glasses", "Telescope", "Violin", "Candle", "Castle",
+                "Church", "Clock", "Globe", "Sculpture", "Thinker", "Old man", "Grandparent"
+            )
+        )
+
+        for (item in picList) {
+            for ((personality, labels) in personalities) {
+                println(item)
+                if (labels.contains(item)) {
+                    curScore[personality] += 1
+                }
+            }
+        }
+    }
+
 }
