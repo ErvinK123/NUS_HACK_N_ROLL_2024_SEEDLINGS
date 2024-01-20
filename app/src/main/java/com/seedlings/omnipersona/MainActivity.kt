4258a93125dcec31a42a3ffdfe76032831ebc403
Ervin Kin <ervinkin@yahoo.com.sg>
2024-01-20 16:17:51 +0800

    Add ViewModel

--- app/src/main/java/com/seedlings/omnipersona/MainActivity.kt
+++ app/src/main/java/com/seedlings/omnipersona/MainActivity.kt
@@ -2,11 +2,15 @@ package com.seedlings.omnipersona
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
+import com.seedlings.omnipersona.storage.ApplicationViewModel
 import com.seedlings.omnipersona.view.fragments.WelcomeFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel : ApplicationViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
