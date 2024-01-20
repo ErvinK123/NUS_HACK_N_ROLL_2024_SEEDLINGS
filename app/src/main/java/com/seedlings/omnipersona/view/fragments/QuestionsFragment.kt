4a9ffc727d72626c7ed8df77d9db71ba33742d0d
eesuan <eesuan.234@gmail.com>
2024-01-21 00:51:27 +0800

    Add questions

--- app/src/main/java/com/seedlings/omnipersona/view/fragments/QuestionsFragment.kt
+++ app/src/main/java/com/seedlings/omnipersona/view/fragments/QuestionsFragment.kt
@@ -120,7 +120,7 @@ class QuestionsFragment(val scores: MutableList<Int>, val counter: Int) : Fragme
             println("scores to add: $scoresToAdd")
             println("scores now: $curScore")
             parentFragmentManager.commit {
-                if (questionCounter > 2) {
+                if (questionCounter > 4) {
                     System.out.println("I CHANGED TO CAMERA ")
                     replace(R.id.frameLayout, CameraFragment(curScore))
                     return@commit
